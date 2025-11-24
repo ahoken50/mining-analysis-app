ca2718e23c03cd5deaccc14040c61efef37a92c0
ahoken50 <micross50@gmail.com>
2025-11-23 21:01:59 -0500

    Fix Angular build errors: syntax, imports, and templates

--- mining-analysis-app/src/app/app.ts
+++ mining-analysis-app/src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ToastComponent } from './shared/components/toast/toast.component';
 
