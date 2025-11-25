8e09ef222cc5f6eafd0d36e40fced95645cb8a1c
ahoken50 <micross50@gmail.com>
2025-11-24 22:46:02 -0500

    feat: Add environment configuration for production and development.

--- mining-analysis-app/src/environments/environment.prod.ts
+++ mining-analysis-app/src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
         apiKey: "YOUR_API_KEY",
         authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
         projectId: "YOUR_PROJECT_ID",
-        storageBucket: "YOUR_PROJECT_ID.appspot.com",
+        storageBucket: "mining-analysis-app.firebasestorage.app",
         messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
         appId: "YOUR_APP_ID"
     },
